41d0c8a44c455d7cf1cbd467d5110a5afe23fe10
Ricardo Perez <ararage@ricardos-macbook-pro.local>
2017-01-26 22:08:55 -0600

    Initial Commit

---
+++ Algebra Multiplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Algebra Multiplication
+//
+//  Created by Ricardo Perez on 1/26/17.
+//  Copyright © 2017 Ricardo Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
